6748f8d0b80c211ca40fe02dc32ecc55ec8a591a
zogoo <chtsogbadrakh@gmail.com>
2021-11-02 19:01:11 +0900

    More messages

--- motor_node.py
+++ motor_node.py
@@ -9,9 +9,12 @@ class MotorNode:
     HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
     PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
     def __init__(self) -> None:
+        print("Opening socket")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.HOST, self.PORT))
+        print("Start to listen")
         self.socket.listen()
+        print("Accepting socket")
         self.conn, self.addr = self.socket.accept()
         print("Connected by", self.addr)
 

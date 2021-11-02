47d5f279201e4d98ec2f6f65ba9b4664ef873a6a
zogoo <chtsogbadrakh@gmail.com>
2021-11-02 22:40:04 +0900

    Simple tcp connection (#2)
    Communicate between modules via TCP socket connection.

---
+++ camera_node.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import socket
+from Camera import Camera
+import json
+
+class CameraNode:
+    HOST = "127.0.0.1"  # The server's hostname or IP address
+    PORT = 65432  # The port used by the server
+    
+    def __init__(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        try:
+            self.socket.close()
+        except RuntimeWarning:
+            return True
+
+    def _dict_to_bytes(self, dict):
+        return json.dumps(dict, ensure_ascii=False).encode('utf-8')
+
+    def connect_to_host(self):
+        self.socket.connect((self.HOST, self.PORT))
+
+    def send_command(self, data):
+        self.socket.sendall(self._dict_to_bytes(data))
+        resp = self.socket.recv(1024)
+        print("Received", repr(resp))
+
+cnode = CameraNode()
+cnode.connect_to_host()
+
+def find_object(results, labels, sizes, distances, obj_name):
+    score = 0
+    obj_name_test = 'person'
+    # print("Result: ", repr(results), "Labels:", repr(labels))
+
+    for obj in results:
+        print("Found object: ", labels[obj['class_id']])
+        if labels[obj['class_id']] == obj_name_test:
+            score = obj['score']
+
+    obj_size = next((size['pixel_metric']
+                    for size in sizes if size["name"] == obj_name_test), 0)
+    obj_dist = next((dist['focal_distance']
+                    for dist in distances if dist["name"] == obj_name_test), 0)
+
+    print(obj_name_test, " is located far from ", round(obj_dist, 1), " and size is", round(obj_size, 1))
+    print("Score is ", score)
+
+    if score < 0.5:
+        print("Finding", obj_name_test, "that detected 50 % more percents: ", score)
+        # cnode.send_command({"action": "left", "value": "4"}) # Have some issue with left side gear
+        cnode.send_command({"action": "right", "value": "4"})
+    elif score > 0.5:
+        print("Stopping all movements")
+        cnode.send_command({"action": "stop", "value": "0"})
+
+    if score > 0.5 and obj_dist < 1150:
+        print("Trying to reach near as possible: ", obj_dist)
+        cnode.send_command({"action": "forward", "value": "1"})
+    elif score > 0.5 and obj_dist < 1150:
+        print("Stopping all movements")
+        cnode.send_command({"action": "stop", "value": "0"})
+
+    if score > 0.5 and obj_dist < 1150 and obj_size > 7:
+        print("Try to catch with shovel")
+        cnode.send_command({"action": "shovel-down", "value": "5"})
+
+tl_models = [
+    {
+        'name': 'shovel',
+        'model_path': './trained_model/shovel_model/model.tflite',
+        'label_path': './trained_model/shovel_model/model-dict.txt',
+        'function': None
+    },
+    {
+        'name': 'person',
+        'model_path': './trained_model/object/detect.tflite',
+        'label_path': './trained_model/object/coco_labels.txt',
+        'function': find_object
+    }
+]
+
+camera = Camera(tl_models)
+camera.execute_command()
+cnode.__exit__()
---
+++ motor_node.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+from Excavator import Excavator
+import json
+import socket
+import io
+
+class MotorNode:
+    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+    def __init__(self) -> None:
+        print("Opening socket")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.HOST, self.PORT))
+        print("Start to listen")
+        self.socket.listen()
+        print("Accepting socket")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.socket.close()
+        except RuntimeWarning:
+            return True
+
+    def _binary_to_dict(bin_str):
+        jsn = ''.join(chr(int(x, 2)) for x in bin_str.split())
+        d = json.loads(jsn)
+        return d
+
+    def _binary_to_dict(self, json_bytes):
+        tiow = io.TextIOWrapper(
+            io.BytesIO(json_bytes), encoding='utf-8', newline=""
+        )
+        obj = json.load(tiow)
+        tiow.close()
+        return obj
+
+    def listen_commands(self, instructions):
+        print("Waiting instructions")
+        try:
+            conn, addr = self.socket.accept()
+            print("Connected by", addr)
+            with conn:
+                while True:
+                    self.data = conn.recv(1024)
+                    if not self.data:
+                        break
+                    dict_data = self._binary_to_dict(self.data)        
+                    action = dict_data.get("action")
+                    query = dict_data.get("value")
+                    print("Got instruction from client and going to execute it: ", action)
+                    instructions.get(action)['cmd']()
+                    if bool(instructions.get(action)['exec']):
+                        instructions.get(action)['exec'](int(query))
+                    conn.sendall(b"ok")
+
+        except KeyboardInterrupt:
+            print("caught keyboard interrupt, exiting")
+        finally:
+            self.socket.close()
+
+excavator = Excavator()
+instructions = {
+    "forward": {'cmd': excavator.move_forward, 'exec': excavator.execute},
+    "backward": {'cmd': excavator.move_backward, 'exec': excavator.execute},
+    "left": {'cmd': excavator.forward_left_chain, 'exec': excavator.execute},
+    "right": {'cmd': excavator.forward_right_chain, 'exec': excavator.execute},
+    "shovel-left": {'cmd': excavator.turn_left_body, 'exec': excavator.execute},
+    "shovel-right": {'cmd': excavator.turn_right_body, 'exec': excavator.execute},
+    "shovel-up": {'cmd': excavator.move_up_shovel, 'exec': excavator.execute},
+    "shovel-down": {'cmd': excavator.move_down_shovel, 'exec': excavator.execute},
+    "stop": {'cmd': excavator.stop_all_motors, 'exec': None}
+}
+
+motor_node = MotorNode()
+motor_node.listen_commands(instructions)
